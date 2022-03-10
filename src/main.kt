7893b10ad09e07db7f912b8c833777fc928574ae
Wanjiru-Kamau <njirukmaureen@gmail.com>
2022-03-10 08:49:45 +0300

    assesment no2

---
+++ src/main.kt
@@ -0,0 +1,23 @@
+fun main(){
+    var course = ("codeHive".tostring)
+    course[4]
+    course[5]
+    course[6]
+    course[7]
+    println(course)
+    modulus(15 , 2)
+    stmnt()
+
+}
+fun main(a:Int, b: Int){
+    var modulus = a % b
+    println(modulus)
+
+}
+fun main(){
+    var stmnt = "Hi my name is $ i am $ years old"
+    var name = "maureen"
+    var age = 19
+    println(stmnt)
+
+}
